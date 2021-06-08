68e883be1562eb742f926e963302e46f01b49e45
Mediogre <caseyarrington13@gmail.com>
2021-06-07 19:26:33 -0500

    finish pizza model

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const htmlRoutes = require('./html/html-routes');
 
-router.use('/', apiRoutes);
+router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
